bd1c3b6ddf0b213b8a1e8da0681298b0672f0163
Hakan ÖZER <hakanozer02@gmail.com>
2016-12-25 11:55:27 +0300

    Proje Başlangıç

---
+++ storeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  storeApp
+//
+//  Created by Hakan on 25/12/2016.
+//  Copyright © 2016 Hakan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
